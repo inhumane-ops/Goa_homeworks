45b25d3a2c87a9da7b08cab6d8da2e3bd76031bf
inhumane-ops <dogepipia41575@gmail.com>
2025-09-12 13:51:26 +0400

    განახლება

---
+++ day 1/homework/lesson.py
@@ -0,0 +1,31 @@
+name = "temo"
+
+print(name)
+
+name = "luka"
+
+print(name)
+
+name = "nona"
+
+print(name)
+
+name = "anzori"
+
+print(name)
+
+name = "dachi" 
+
+print(name)
+
+age = 15
+
+print(age)
+
+country = "georgia"
+
+print(country)
+
+price = 4.99
+
+print(price)
---
+++ day 5/homework/python.py
@@ -0,0 +1,42 @@
+print(my_university)
+
+
+name1 = "Luka"
+age_num = 56
+my_fav_mov = "Figther in the wind იმენა ვოვ რა უნდა უყუროთ absolute cinema"
+my_friends_name = "Nika"
+
+print("My name is " + name1)
+print("My age is " + str(age_num))
+print("My Favorite Movie is " + my_fav_mov)
+print("My Friend's name is " + my_friends_name)
+
+
+first_name = "Luka"
+last_name = "merabi!"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+
+city = "Tbilisi"
+country = "Georgia"
+location = city + " " + country
+print(location)
+
+food = "xinkali"
+drink = "Lemonade"
+menu = food + " " + drink
+print(menu)
+
+brand = "Apple"
+product = "iPhone"
+device = brand + " " + product
+print(device)
+
+greeting = "Hello"
+name = "Ana"
+message = greeting + " " + name
+print(message)
+
+# snake_case არის სტილი  სადაც ცვლადის სახელში ყველა ასო პატარაა  და სიტყვები ერთმანეთისგან გამოყოფილია ქვედა ტირეთი როგორცა ზდაროვა_სვერჩიკ  შე_კარტოფილის_გამყიდველო ნასკი_ნასკდოლარე და ასე შემდეგ
